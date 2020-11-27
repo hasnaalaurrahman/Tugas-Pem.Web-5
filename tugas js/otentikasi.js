32775d362207658b4eefa4540a1a313cec0c8448
hasnaalaurrahman <73053584+hasnaalaurrahman@users.noreply.github.com>
2020-11-27 23:53:29 +0700

    0110120161-Hasna Alaurrahman-SI03

---
+++ tugas js/otentikasi.js
@@ -0,0 +1,9 @@
+function otentikasi() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    if (user=="hasnaalrr" && pass==asdfghjkl){
+        return loginsukses.html;
+    }else{
+        alert("Data tidak valid!");
+    } 
+}
